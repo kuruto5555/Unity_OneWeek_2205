05f4612ab038eaaa19220e1d3b950cde2b75b91d
arairyota <bythx790@gmail.com>
2022-05-04 16:35:13 +0900

    スワイプ実装 ブランチ名はミスっちゃった 正：Swipe

--- Assets/Script/Input/MyInput.cs
+++ Assets/Script/Input/MyInput.cs
@@ -7,12 +7,38 @@ public class MyInput : MonoBehaviour, Unity_OneWeek_2205.IPlayerActions
 {
     public static MyInput _instance { get; private set; }
 
+    [Header("スワイプ判定になるまでの距離")]
+    [SerializeField] float SwipeLen = 1.0f;
+
+
+
+    /// <summary>
+    /// マウスの現在の座標
+    /// </summary>
     public Vector3 MousePos { get; private set; } = Vector3.zero;
 
+    /// <summary>
+    /// タッチされた時の開始座標
+    /// </summary>
+    public Vector3 StartMousePos { get; private set; } = Vector3.zero;
 
+    /// <summary>
+    /// スワイプ判定に使う長さ
+    /// </summary>
+    private float _swipeLen = 0.0f;
 
 
     private Unity_OneWeek_2205 _input = null;
+
+    /// <summary>
+    /// スワイプされたかどうか
+    /// </summary>
+    private bool _isSwipe = false;
+
+    /// <summary>
+    /// 画面に触れているかどうか
+    /// </summary>
+    private bool _isTouch = false;
 
     private void Awake()
     {
@@ -42,13 +68,29 @@ public class MyInput : MonoBehaviour, Unity_OneWeek_2205.IPlayerActions
         // Start is called before the first frame update
     void Start()
     {
-        
+        _isSwipe = false;
+        _isTouch = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StateUpdate();
+    }
+
+    private void StateUpdate()
+    {
+        if (!_isTouch) return;
+
+        MousePos = Input.mousePosition;
+
+        _swipeLen = Vector3.Distance(MousePos, StartMousePos);
+
+        if(SwipeLen < _swipeLen)
+        {
+            _isSwipe = true;
+            Debug.Log("スワイプの判定になったで");
+        }
     }
 
   
@@ -59,6 +101,9 @@ public class MyInput : MonoBehaviour, Unity_OneWeek_2205.IPlayerActions
         {
             Debug.Log("押されたお");
 
+            StartMousePos = Input.mousePosition;
+
+            _isTouch = true;
         }
 
         //タップ判定の瞬間
@@ -72,6 +117,9 @@ public class MyInput : MonoBehaviour, Unity_OneWeek_2205.IPlayerActions
         if (context.canceled)
         {
             Debug.Log("離されたお");
+
+            _isTouch = false;
+            _isSwipe = false;
         }
     }
 }
