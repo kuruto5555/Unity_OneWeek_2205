e96123115bcce77e8fb84b36bcbd226383ec76dc
arairyota <bythx790@gmail.com>
2022-05-05 00:20:24 +0900

    MoveController準備（途中）

---
+++ Assets/Script/MoveController/MoveController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public static MoveController _instance { get; private set; }
+
+    private bool _isFrameTouch = false;
+
+
+
+    private void Awake()
+    {
+        if (_instance == null) _instance = this;
+        else
+        {
+            Destroy(this);
+            Debug.LogError("MoveContollerはすでに別オブジェクトにアタッチされていた為破棄されました。アタッチされているObjectは" + _instance.gameObject.name);
+            return;
+        }
+    }
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        _isFrameTouch = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PutItem()
+    {
+        if (!_isFrameTouch) return;
+    }
+}
