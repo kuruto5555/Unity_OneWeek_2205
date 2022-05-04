9b18f73f6664f5534ad13b7c66645d846f138c03
BANDIT <guruxeon86@gmail.com>
2022-05-04 15:45:54 +0900

    タイマー実装（要調整事項あり）

---
+++ Assets/Script/Timer/TimerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerController : MonoBehaviour
+{
+    public Text timerText;
+    public float timeLimit;  // 制限時間
+    float timeCounter;       // 内部処理用
+    int seconds;
+    bool countActiveFlag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter = timeLimit;
+        countActiveFlag = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countActiveFlag == true)
+        {
+            timeCounter -= Time.deltaTime;
+            seconds = (int)timeCounter;
+            timerText.text = seconds.ToString();
+        }
+
+    }
+
+    // 時間切れ判定
+    void TimeOverChecker()
+    {
+        if (seconds >= 0)
+        {
+
+        }
+    }
+
+}
