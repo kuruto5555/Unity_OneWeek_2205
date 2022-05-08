45dcd9d3caef18a722c5825c046850f00a00eff7
yuyaoyukiデスクトップPC <h.kuruto.0505@gmail.com>
2022-05-08 16:35:01 +0900

    ResultScene完成

---
+++ Assets/Script/UI/Result/ToTitleScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using BTLGeek.Manager;
+using BTLGeek.Constants;
+
+namespace BTLGeek.UI
+{
+	public class ToTitleScene : MonoBehaviour
+	{
+		public void OnClick()
+		{
+			ResultSceneController.Instance.FadeStart(SceneName.TITLE_SCENE, FADE_TYPE.FADE_OUT);
+			SoundManager.Instance.PlaySeByName(SEPath.DECISION5);
+			EventSystem.current.gameObject.SetActive(false);
+		}
+	}
+}
